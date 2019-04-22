ee617ec67dadd78ed2561c7f34e9885db0bd0ba4
Yoshin Govender <yoshin.govender@gmail.com>
2019-04-22 16:19:46 -0400

    Changed code_generator script into a single class

--- code_generator.py
+++ code_generator.py
@@ -1,31 +1,34 @@
 from pprint import pprint
 from itertools import compress
 
-timers = {
-    'UNO':{
-        0:(8,['CS02','CS01','CS00']),
-        1:(16,['CS12','CS11','CS10']),
-        2:(8, ['CS22','CS21','CS20'])
-        }
-    }
+class Generator:
 
-prescalers = [1, 8, 64, 256, 1024]
+    def __init__(self):
+        self.timers = {
+            'UNO':{
+                0:(8,['CS02','CS01','CS00']),
+                1:(16,['CS12','CS11','CS10']),
+                2:(8, ['CS22','CS21','CS20'])
+                }
+            }
+        self.prescalers = [1, 8, 64, 256, 1024]
 
-ctc_regs = ["WGM01","WGM12","WGM21"]
+        self.ctc_regs = ["WGM01","WGM12","WGM21"]
 
-ctc_strings = ["TCCR0A |= (1 << WGM01)",
-               "TCCR1B |= (1 << WGM12)",
-               "TCCR2A |= (1 << WGM21)"]
+        self.ctc_strings = [
+            "TCCR0A |= (1 << WGM01)",
+            "TCCR1B |= (1 << WGM12)",
+            "TCCR2A |= (1 << WGM21)"]
 
-clock_bits = {
-    1:[0,0,1],
-    8:[0,1,0],
-    64:[0,1,1],
-    256:[1,0,0],
-    1024:[1,0,1]
-    }
+        self.clock_bits = {
+            1:[0,0,1],
+            8:[0,1,0],
+            64:[0,1,1],
+            256:[1,0,0],
+            1024:[1,0,1]
+            }
 
-template = """
+        self.template = """
 //set timer{timer} interrupt at 1Hz
   TCCR{timer}A = 0;// set entire TCCR1A register to 0
   TCCR{timer}B = 0;// same for TCCR1B
@@ -40,39 +43,37 @@ template = """
   TIMSK{timer} |= (1 << OCIE{timer}A);
 """
 
+    def generateScales(self, interrupt_frequency, board='UNO', clock_frequency=16e6):
+        clock_resolution = 1 / clock_frequency
+        output = {}
+        for timer, res in self.timers[board].items():
+            output[timer] = []
+            for value in self.prescalers:
+                timer_resolution = value * clock_resolution
+                time_counts = int((1 / interrupt_frequency) / timer_resolution) - 1
+                if time_counts < 2 ** res[0]:
+                    output[timer].append((value, time_counts))
+        return output
 
+    def generatePrescaleCode(self, timer, prescaler):
+        preSclRegs = []
+        for i in range(2, -1, -1):
+            preSclRegs.append("CS{}{}".format(timer, i))
 
-def generateScales(interrupt_frequency,board='UNO',clock_frequency=16e6):
-    clock_resolution = 1/clock_frequency
-    output = {}
-    for timer,res in timers[board].items():
-        output[timer] = []
-        for value in prescalers:
-            timer_resolution = value*clock_resolution
-            time_counts = int((1/interrupt_frequency)/timer_resolution) - 1
-            if time_counts < 2**res[0]:
-                output[timer].append((value,time_counts))
-    return output
+        foo = list(compress(preSclRegs, self.clock_bits[prescaler]))
+        bar = " | ".join(["(1 << {})".format(item) for item in foo])
+        return bar
 
-def generatePrescaleCode(timer,prescaler):
-    preSclRegs = []
-    for i in range(2,-1,-1):
-        preSclRegs.append("CS{}{}".format(timer,i))
-
-    foo = list(compress(preSclRegs, clock_bits[prescaler]))
-    bar = " | ".join(["(1 << {})".format(item) for item in foo])
-    return bar
-
-def generateCode(timer,prescalar,count_val):
-    setup_code = template.format(
-        timer = timer,
-        prescale_str = generatePrescaleCode(timer,prescalar),
-        count_val = count_val,
-        ctc_string = ctc_strings[timer],
-        prescalar = prescalar
+    def generateCode(self, timer, prescalar, count_val):
+        setup_code = self.template.format(
+            timer=timer,
+            prescale_str=self.generatePrescaleCode(timer, prescalar),
+            count_val=count_val,
+            ctc_string=self.ctc_strings[timer],
+            prescalar=prescalar
         )
 
-    code = """
+        code = """
 cli(); //stop interrupts
 {setup_code}
 sei(); //allow interrupts
@@ -80,20 +81,20 @@ sei(); //allow interrupts
 ISR(TIMER{timer}_COMPA_vect){{
     // Do Something
 }}
-    """
-    return code.format(setup_code=setup_code,
-                       timer=timer)
+        """
+        return code.format(setup_code=setup_code,
+                           timer=timer)
 
 def Main(ISR_Freq,board='UNO'):
+    generator = Generator()
     print("Here are the options:\n")
-    options = generateScales(ISR_Freq)
+    options = generator.generateScales(ISR_Freq)
     pprint(options)
     timer = int(input("Enter Selected Timer: "))
     prescalar = int(input("Enter Selected Prescalar: "))
-    code = generateCode(timer,prescalar,dict(options[timer])[prescalar])
+    code = generator.generateCode(timer,prescalar,dict(options[timer])[prescalar])
     print(code)
-    
-    
+
 if __name__ == '__main__':
     Desired_Interrupt_Frequency = 2
     Main(2)
